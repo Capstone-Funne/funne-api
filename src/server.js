f05e5ede274255352cce7312395ddf4ef19ca740
edwintantawi <edwintantawi.workspace@gmail.com>
2023-05-24 14:14:04 +0700

    create login user feature

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { v4: uuid } = require('uuid');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const { database } = require('./database');
 
@@ -65,11 +66,79 @@ app.post('/users', async (req, res) => {
         password: hashPassword,
       },
     });
-    return res
-      .status(201)
-      .json({ status_code: 201, message: 'Registrasi berhasil', data: null });
+    return res.status(201).json({
+      status_code: 201,
+      message: 'Registrasi berhasil',
+      data: null,
+    });
   } catch (error) {
-    console.error(JSON.stringify(error));
+    console.error(JSON.stringify(error.message));
+    return res.status(500).json({
+      status_code: 500,
+      message: 'Internal Server Error',
+      error: 'Maaf ada sesuatu yang salah di server',
+    });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const payload = req.body;
+
+  try {
+    if (!payload.email?.match(EMAIL_REGEX)) {
+      return res.status(400).json({
+        status_code: 400,
+        message: 'Bad Request',
+        error: 'Masukkan email yang valid',
+      });
+    }
+
+    if (!payload.password) {
+      return res.status(400).json({
+        status_code: 400,
+        message: 'Bad Request',
+        error: 'Wajib memasukkan password',
+      });
+    }
+
+    const user = await database.user.findFirst({
+      where: { email: payload.email },
+    });
+
+    const isPasswordMatch = await bcrypt.compare(
+      payload.password,
+      user?.password ?? ''
+    );
+
+    if (user === null || !isPasswordMatch) {
+      return res.status(400).json({
+        status_code: 400,
+        message: 'Bad Request',
+        error: 'Email atau password salah',
+      });
+    }
+
+    const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '10d',
+    });
+
+    return res.status(200).json({
+      status_code: 200,
+      message: 'Berhasil login',
+      data: {
+        access_token: accessToken,
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        picture:
+          user.picture ??
+          encodeURI(
+            `https://api.dicebear.com/6.x/initials/svg?seed=${user.name}`
+          ),
+      },
+    });
+  } catch (error) {
+    console.error(JSON.stringify(error.message));
     return res.status(500).json({
       status_code: 500,
       message: 'Internal Server Error',

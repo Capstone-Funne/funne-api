3f4411a9a4bfc0e5741265774644521ccd03fd12
edwintantawi <edwintantawi.workspace@gmail.com>
2023-05-25 22:37:02 +0700

    create express error middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const { ClientError } = require('./exception/client-error');
+
 function authMiddleware(req, res, next) {
   const bearerToken = req.headers.authorization;
 
@@ -50,4 +52,24 @@ function authMiddleware(req, res, next) {
   }
 }
 
-module.exports = { authMiddleware };
+// Express error middleware
+// Reference: https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+// eslint-disable-next-line no-unused-vars
+function errorMiddleware(error, req, res, next) {
+  if (error instanceof ClientError) {
+    return res.status(error.statusCode).json({
+      status_code: error.statusCode,
+      message: error.message,
+      data: null,
+    });
+  }
+
+  console.error(error);
+  return res.status(500).json({
+    status_code: 500,
+    message: 'Internal Server Error',
+    data: null,
+  });
+}
+
+module.exports = { authMiddleware, errorMiddleware };

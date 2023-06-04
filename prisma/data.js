aa2a04516e91020aadcd669fd016c48449120089
Puan Abidah Nitisara <puanabidah13@gmail.com>
2023-06-04 21:44:43 +0700

    add product seed

---
+++ prisma/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: '840c7f3c-1bd2-433c-953c-7084c9d69e9f',
+    name: 'Cetaphil Daily Facial Cleanser',
+    category: 'Face Wash',
+    picture: '1-Cetaphil-Daily-Facial-Cleanser.jpg',
+    description:
+      'Pembersih wajah lembut dan efektif untuk semua jenis kulit. Kami menganggap produk ini aman untuk individu yang menderita jerawat jamur. Kesimpulan ini didasarkan pada bahan-bahan yang tercantum di atas dan penelitian kami saat ini tentang topik tersebut. Namun, pengujian patch sebelum digunakan sangat disarankan. Jika terjadi iritasi, hentikan penggunaan dan segera konsultasi ke dokter spesialis kulit.',
+    ingredients:
+      'Water,Cocamidopropyl Betaine,Disodium Laureth Sulfosuccinate,Sodium Cocoamphoacetate,Panthenol,Niacinamide,Pantolactone,Acrylates/C10-30 Alkyl Acrylate Crosspolymer,Sodium Benzoate,Fragrance,Sodium Chloride,Citric Acid',
+  },
+  {
+    id: '65e39944-0fa0-40ac-a883-e4ff75df43d5',
+    name: 'Cosrx AHA 7 Whitehead Power Liquid',
+    category: 'Exfoliating Toner',
+    picture: '2-Cosrx-AHA-7-Whitehead-Power.jpg',
+    description:
+      'Cairan AHA untuk mengatasi komedo putih dan meratakan tekstur kulit. Kesimpulan ini didasarkan pada bahan-bahan yang tercantum di atas dan penelitian kami saat ini tentang topik tersebut. Namun, pengujian patch sebelum digunakan sangat disarankan. Jika terjadi iritasi, hentikan penggunaan dan segera konsultasi ke dokter spesialis kulit.',
+    ingredients:
+      'Blemish & Acne Treatments,Exfoliant,Face Skincare,Treatments","Pyrus Malus (Apple) Fruit,Water,Butylene Glycol,Glycolic Acid,Niacinamide, Sodium Hydroxide,Sodium Hydroxide,1,2-Hexanediol,Panthenol',
+  },
+  {
+    id: '1fd9b872-1bbc-4415-98b7-80d0da86f0fa',
+    name: 'La Roche-Posay Toleriane Sensitive Fluide Protective Moisturizer',
+    category: 'Moisturizer',
+    picture: '3-La-Roche-Posay-Toleriane-Moisturizer.jpg',
+    description:
+      'Pelembap pelindung khusus kulit sensitif. Kesimpulan ini didasarkan pada bahan-bahan yang tercantum di atas dan penelitian kami saat ini tentang topik tersebut. Namun, pengujian patch sebelum digunakan sangat disarankan. Jika terjadi iritasi, hentikan penggunaan dan segera konsultasi ke dokter spesialis kulit.',
+    ingredients:
+      'Capric Triglyceride,Caprylyl Glycol,Citric Acid,Glycerin,Niacinamide,Pentylene Glycol,Propanediol,Vitamin B3',
+  },
+];

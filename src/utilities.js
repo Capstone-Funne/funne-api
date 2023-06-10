6c3d07a41d0054a9423d17445dc36d17b41df440
edwintantawi <edwintantawi.workspace@gmail.com>
2023-06-10 18:02:58 +0700

    change picture format from svg to png

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,3 @@
 export function getPictureByName(name) {
-  return encodeURI(`https://api.dicebear.com/6.x/initials/svg?seed=${name}`);
+  return encodeURI(`https://api.dicebear.com/6.x/initials/png?seed=${name}`);
 }
